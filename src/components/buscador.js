7eea8cba3aec167931593e313750bd5fd600d2ae
Juan Antonio Hernández Rodríguez <158836150+ULPGCJuan@users.noreply.github.com>
2024-03-02 20:34:21 +0000

    Buscador js

---
+++ src/components/buscador.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/buscador.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+function Buscador() {
+  return (
+    <section className="up-header"> 
+      <input type="text" className="search-input" placeholder="Realizar búsqueda..." />
+      <button className="submit">
+        <FontAwesomeIcon icon={faSearch} />
+      </button>
+    </section>
+  );
+}
+
+export default Buscador;
