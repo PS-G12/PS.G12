f2b7c5c9f61d9e416c5d904cb61e86087b8d0ddd
acaymo <acaimopcgc@gmail.com>
2024-03-03 23:15:40 +0000

    Click en tarjetas de rutinas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Rutinas from './pages/rutina/rutinas';
 import SearchResultsPage from './pages/resultadosBusqueda/resultadosBusqueda';
+import ExercisePage from './pages/exercise/exercise';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/rutinas" element={<Rutinas />} />
         <Route path="/resultados" element={<SearchResultsPage />} /> 
+        <Route path="/exercise" element={<ExercisePage />} />
       </Routes>
     </Router>
   );
