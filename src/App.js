195e1d19bce8c0e16ff4fd5e533c1be3f4d0a62c
Santiago <pimisan@gmail.com>
2024-03-04 00:05:45 +0000

    Funcionalidad de la calculadora IMC añadida y página /pages/imc.js montada

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Rutinas from './pages/rutinas';
+import CalculoIMCPage from './pages/imc';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/rutinas" element={<Rutinas />} />
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/rutinas" element={<Rutinas />} />
+        </Routes>
+      </Router>
+      <CalculoIMCPage /> //Esto se puede quitar
+    </div>
   );
 }
 
