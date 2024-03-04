f4acee8f00e10624887c26919386597fee6d9e8f
acaymo <acaimopcgc@gmail.com>
2024-03-04 19:06:01 +0000

    Corrección de CSS

--- src/pages/exercise/exercise.js
+++ src/pages/exercise/exercise.js
@@ -13,14 +13,18 @@ const ExercisePage = () => {
 
   return (
     <div className="container">
-      <h1>Exercise Details</h1>
+
       <div className="exercise-details">
-        <h2>Name: <span>{exercise.name}</span></h2>
+        <div className="img-name">
+          <h1><span>{exercise.name}</span></h1>
+          <img src={require(`../../gifs/${exercise.id}.gif`)} alt={exercise.name} />
+        </div>
+        
         <p><strong>Body Part:</strong> <span>{exercise.bodyPart}</span></p>
         <p><strong>Equipment:</strong> <span>{exercise.equipment}</span></p>
         <p><strong>Target:</strong> <span>{exercise.target}</span></p>
         <p><strong>Secondary Muscles:</strong> <span>{exercise.secondaryMuscles.join(', ')}</span></p>
-        <img src={require(`../../gifs/${exercise.id}.gif`)} alt={exercise.name} />
+        
         <div className="instructions">
           <h3>Instructions:</h3>
           <ul>
