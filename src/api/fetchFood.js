0b6a7ad9dbbf4448f62d81b1b1e8c1de57a977d8
Javi9davi <javi9davi@gmail.com>
2024-03-05 11:17:44 +0000

    fetchFood y buscarAlimento

--- src/api/fetchFood.js
+++ src/api/fetchFood.js
@@ -1,7 +1,7 @@
-function fetchFood(query) {
+async function fetchFood(query) {
     const apiKey = 'b2GaD1UR7/oiKhAhIIDz+g==CPqBrb1RharNi8wE';
 
-    fetch(`https://api.calorieninjas.com/v1/nutrition?query=${encodeURIComponent(query)}`, {
+    await fetch(`https://api.calorieninjas.com/v1/nutrition?query=${encodeURIComponent(query)}`, {
         method: 'GET',
         headers: {
             'X-Api-Key': apiKey,
@@ -23,5 +23,4 @@ function fetchFood(query) {
     });
 }
 
-// Example usage
-fetchFood('3lb carrots and a chicken sandwich');
+export default fetchFood;
