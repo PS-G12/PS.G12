a95f3b68f3e810c616bd85a478fa580bbe98085e
acaymo <acaimopcgc@gmail.com>
2024-03-09 19:45:28 +0000

    pagina inicio

--- src/components/TarjetaObjetivo/tarjetaObjetivo.js
+++ src/components/TarjetaObjetivo/tarjetaObjetivo.js
@@ -4,12 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFlag, faUtensils, faRunning } from '@fortawesome/free-solid-svg-icons'
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 
-function TarjetaObjetivo(){
+function TarjetaObjetivo({restantes, objetivoKcal, alimento, ejercicio, value}) {
     return (
         <div className='container-tarjeta'>
             <div className='progresoCircular'>
-                <CircularProgressbar value={65} text='1502 kcal restantes' styles={buildStyles({ 
-                    textSize: '12px',
+                <CircularProgressbar value={value} text={`${restantes} kcal restantes`} styles={buildStyles({ 
+                    textSize: '10px',
                     pathTransitionDuration: 0.5,
                     pathColor: 'red',
                     textColor: 'black',
@@ -22,17 +22,17 @@ function TarjetaObjetivo(){
                 <div className='kcal'>
                     <FontAwesomeIcon icon={faFlag} className='icon'/>
                     <span>Objetivo Kcal</span>
-                    <p>2542</p>
+                    <p>{objetivoKcal}</p>
                 </div>
                 <div className='alimento'>
                     <FontAwesomeIcon icon={faUtensils} className='icon'/>
                     <span>Alimento</span>
-                    <p>0</p>
+                    <p>{alimento}</p>
                 </div>
                 <div className='ejercicio'>
                     <FontAwesomeIcon icon={faRunning} className='icon'/>
                     <span>Ejercicio</span>
-                    <p>0</p>
+                    <p>{ejercicio}</p>
                 </div>
             </div>
         </div>
