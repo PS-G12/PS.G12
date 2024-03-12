adda898e69ea53fbef59b7ed6095beab137c9235
Acaymo Granado Sánchez <acaimopcgc@gmail.com>
2024-03-12 12:41:50 +0100

    Cambio de errores

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -14,7 +14,7 @@ const indexPage = () => {
       <div className="index-page">
         <Header />
         <div className="cards">
-          <ObjectiveCard value={(1500/2542)*100} KcalObjective={2542} food={0} exercise={0} remaining={1500} />
+          <ObjectiveCard value={(1500/2542)*100} kcalObjective={2542} food={0} exercise={0} remaining={1500} />
           <MacrosCard value={81} max={165} value2={50} max2={65} value3={32} max3={85}/>
         </div>
 
