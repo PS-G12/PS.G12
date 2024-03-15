bf43aab1229fa8c5d6325e32e9c6dc93468cc251
Javidavi <javi9davi@gmail.com>
2024-03-15 16:29:36 +0000

    Iconos y estilo de la web

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import './header.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChartLine, faUtensils, faDumbbell, faCalculator } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import "./header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChartLine,
+  faUtensils,
+  faDumbbell,
+  faCalculator,
+} from "@fortawesome/free-solid-svg-icons";
+import icon2 from "../../images/icons/icon-3.png";
 
 function Header() {
   const currentPath = window.location.pathname;
@@ -10,17 +16,46 @@ function Header() {
 
   return (
     <header className="header-menu">
+      <div className="icon-bar">
+        <img className="icon-1" src={icon2} alt="Icon" />
+      </div>
       <nav className="navbar">
         <div className="logo"></div>
         <ul className="nav-links">
-          <li className={isActive("/") ? "active" : ""}><a href="/"><FontAwesomeIcon icon={faChartLine} /> HOME</a></li>
-          <li className={isActive("/searchFood") || isActive("/searchFood") ? "active" : ""}><a href="/searchFood"><FontAwesomeIcon icon={faUtensils} /> FOOD</a></li>
-          <li className={isActive("/routines") ? "active" : ""}><a href="/routines"><FontAwesomeIcon icon={faDumbbell} /> EXERCISE</a></li>
+          <li className={isActive("/") ? "active" : ""}>
+            <a href="/">
+              <FontAwesomeIcon icon={faChartLine} /> HOME
+            </a>
+          </li>
+          <li
+            className={
+              isActive("/searchFood") || isActive("/searchFood") ? "active" : ""
+            }
+          >
+            <a href="/searchFood">
+              <FontAwesomeIcon icon={faUtensils} /> FOOD
+            </a>
+          </li>
+          <li className={isActive("/routines") ? "active" : ""}>
+            <a href="/routines">
+              <FontAwesomeIcon icon={faDumbbell} /> EXERCISE
+            </a>
+          </li>
           <li className="dropdown">
-            <a href='#'><FontAwesomeIcon icon={faCalculator} /> CALCULATORS</a>
-            <ul className='submenu'>
-              <li className={isActive("/BMIcalculation") ? "active" : ""}><a href='/BMIcalculation' className='calc'>CALCULATOR BMI</a></li>
-              <li className={isActive("/MacrosCalculation") ? "active" : ""}><a href='/MacrosCalculation' className='calc'>MACROS CALCULATOR</a></li>
+            <a href="#">
+              <FontAwesomeIcon icon={faCalculator} /> CALCULATORS
+            </a>
+            <ul className="submenu">
+              <li className={isActive("/BMIcalculation") ? "active" : ""}>
+                <a href="/BMIcalculation" className="calc">
+                  CALCULATOR BMI
+                </a>
+              </li>
+              <li className={isActive("/MacrosCalculation") ? "active" : ""}>
+                <a href="/MacrosCalculation" className="calc">
+                  MACROS CALCULATOR
+                </a>
+              </li>
             </ul>
           </li>
         </ul>
