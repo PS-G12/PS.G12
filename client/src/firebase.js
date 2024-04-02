b493bcd6e76e0453c12283922a8ae86f9a2a525e
Javi9davi <javi9davi@gmail.com>
2024-04-02 12:19:48 +0000

    Authentication con formularios validados

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -10,10 +10,16 @@ const firebaseConfig = {
     messagingSenderId: "87626998092",
     appId: "1:87626998092:web:4b6b99139acad0f5c6afe9",
     measurementId: "G-T210VZ76YH"
-  };
-  
-  
-  export const app = initializeApp(firebaseConfig);
-  export const auth = getAuth(app);
-  const analytics = getAnalytics(app);
-  
+};
+
+let auth_var = null;
+
+try {
+  const app = initializeApp(firebaseConfig);
+  auth_var = getAuth(app);
+  //const analytics = getAnalytics(app);
+} catch (error) {
+  console.error("Error initializing Firebase:", error);
+}
+
+export const auth = auth_var;
