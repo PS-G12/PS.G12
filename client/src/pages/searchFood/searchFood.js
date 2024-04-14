76264d05840f496208bdc74a3ded11469aed54ba
imano <imanolquinber26@gmail.com>
2024-04-14 23:12:42 +0100

    Cambios en searchFood y RegisterFood

--- client/src/pages/searchFood/searchFood.js
+++ client/src/pages/searchFood/searchFood.js
@@ -77,14 +77,25 @@ const FoodSearch = () => {
     <div className="buscarAlimento-box">
       <Header />
       <h1>Añadir alimento a {type}</h1>
-      <div className="searchbar-container">
-        <input
-          type="text"
-          placeholder="Introduce la búsqueda"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button onClick={handleSearch}>Buscar</button>
+
+      <div className="searchbar">
+        <div id="form" className="searchbar-container">
+          <input
+            type="text"
+            placeholder="Introduce la búsqueda"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <button onClick={handleSearch} class="searchButton">
+            <svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 
+              36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 
+              312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 
+              263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 
+              218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 
+              0-218.455 97.091-218.455 218.455z"></path>
+            </svg>
+          </button>
+        </div>
       </div>
 
       <div className="search-result">
