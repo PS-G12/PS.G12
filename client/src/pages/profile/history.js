f630ad17ff03e02880c647d01fd82669d5a84bc2
Acaymo Granado Sánchez <acaimopcgc@gmail.com>
2024-04-30 13:45:43 +0200

    filtros

--- client/src/pages/profile/history.js
+++ client/src/pages/profile/history.js
@@ -284,6 +284,13 @@ const ProfileHistory = () => {
                             <h1>History</h1>
                             <div className="botones-history">
                                 <FilterByTimeHistory />
+                                <div className = {classBtn} id="btn-download" onClick={exportToExcel}>
+                                <svg width="22px" height="16px" viewBox="0 0 22 16">
+                                    <path d="M2,10 L6,13 L12.8760559,4.5959317 C14.1180021,3.0779974 16.2457925,2.62289624 18,3.5 L18,3.5 C19.8385982,4.4192991 21,6.29848669 21,8.35410197 L21,10 C21,12.7614237 18.7614237,15 16,15 L1,15" id="check"></path>
+                                    <polyline points="4.5 8.5 8 11 11.5 8.5" class="svg-out"></polyline>
+                                    <path d="M8,1 L8,11" class="svg-out"></path>
+                                </svg>
+                            </div>
                                 <a class="credit" href="https://dribbble.com/shots/4570587-Download-micro-interaction" target="_blank"><img src="https://cdn.dribbble.com/assets/logo-footer-hd-a05db77841b4b27c0bf23ec1378e97c988190dfe7d26e32e1faea7269f9e001b.png" alt=""></img></a>
                             </div>
                             <div className="table-container-history">
@@ -336,13 +343,7 @@ const ProfileHistory = () => {
                                     </tbody>
                                 </table>
                             </div>
-                            <div className = {classBtn} id="btn-download" onClick={exportToExcel}>
-                                <svg width="22px" height="16px" viewBox="0 0 22 16">
-                                    <path d="M2,10 L6,13 L12.8760559,4.5959317 C14.1180021,3.0779974 16.2457925,2.62289624 18,3.5 L18,3.5 C19.8385982,4.4192991 21,6.29848669 21,8.35410197 L21,10 C21,12.7614237 18.7614237,15 16,15 L1,15" id="check"></path>
-                                    <polyline points="4.5 8.5 8 11 11.5 8.5" class="svg-out"></polyline>
-                                    <path d="M8,1 L8,11" class="svg-out"></path>
-                                </svg>
-                            </div>
+
                         </div>
                     </div>
                 </div>
